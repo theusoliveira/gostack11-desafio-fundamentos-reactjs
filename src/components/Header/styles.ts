1a95d1fe5706c358aeac36f99dcb4fe8de143057
Matheus Oliveira <matheusoliveira@Matheuss-MacBook-Pro.local>
2020-04-27 19:43:24 -0300

    Css hover

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -28,7 +28,8 @@ export const Container = styled.div<ContainerProps>`
         }
 
         &:hover {
-          opacity: 0.6;
+          border-bottom: 2px solid #ff872c;
+          padding-bottom: 5px;
         }
       }
     }
